6ac009d98ca38e0be589374eeb97133fc968a0a6
r-trevisan <rrhtt3@gmail.com>
2022-03-31 14:44:48 -0400

    mapeadas views para urls

--- Conteudo/views.py
+++ Conteudo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+'''
+Aqui é onde são definidas as Views
+Views é uma função que solicita (request) algo e recebe uma resposta (response). 
+É uma request handler. Arquitetamente falando ela está associada com algo 
+que o usuário vê.
+'''
+
+def diga_ola(request):
+    # aqui podemos pegar/transformar dados de um database, enviar emails, etc...
+    return HttpResponse('Olá Mundão!')
+
+'''
+após definida a função devemos mapear até a URL, para que quando a URL receber
+um request essa função aqui será a resposta(será chamada).
+'''
