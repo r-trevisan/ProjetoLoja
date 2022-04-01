cabc7e39e21567bd0d6bf8b474379854dd57896b
r-trevisan <rrhtt3@gmail.com>
2022-04-01 09:58:37 -0400

    Relacoes OneToOne, OneToMany e ManyToMany

--- armazem/models.py
+++ armazem/models.py
@@ -5,7 +5,18 @@ from django.db import models
 Aplicativos Django acessam e gerenciam dados através de objetos Python chamados de modelos (models).
 Modelos definem a estrutura dos dados armazenados, incluindo os tipos de campos e possivelmente também o seu tamanho máximo,
 valores default, opções de listas de seleção, texto de ajuda para documentação, texto de labels para formulários, etc.
+
+Foram inseridas relações OneToOne, OneToMany e ManyToMany.
 """
+
+class Promocao(models.Model):
+    descricao = models.CharField(max_length=255)
+    desconto = models.FloatField()
+
+
+class Acervo(models.Model):
+    titulo = models.CharField(max_length=255)
+
 
 class Produto(models.Model):
     titulo = models.CharField(max_length=255)
@@ -13,6 +24,9 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=6, decimal_places=2)
     catalogo = models.IntegerField()
     ultimo_update = models.DateTimeField(auto_now=True)
+    acervo = models.ForeignKey(Acervo, on_delete=models.PROTECT) # protege os produtos de um acervo caso você o delete
+    promocoes = models.ManyToManyField(Promocao)
+
 
 class Cliente(models.Model):
     ASSINATURA_BRONZE = 'B'
@@ -33,11 +47,11 @@ class Cliente(models.Model):
         max_length=1, choices=OPCOES_ASSINATURA, default=ASSINATURA_BRONZE
     )
 
+
 class Pedido(models.Model):
     STATUS_PAGAMENTO_PENDENTE = 'P'
     STATUS_PAGAMENTO_COMPLETO = 'C'
     STATUS_PAGAMENTO_FALHOU = 'F'
-    
     OPCOES_PAGAMENTO = [
         (STATUS_PAGAMENTO_PENDENTE, 'Pending'),
         (STATUS_PAGAMENTO_COMPLETO, 'Completo'),
@@ -45,5 +59,30 @@ class Pedido(models.Model):
     ]
     data_do_pedido = models.DateTimeField(auto_now_add=True)
     status_pagamento = models.CharField(
-        max_length=1, choices=OPCOES_PAGAMENTO, default=STATUS_PAGAMENTO_PENDENTE
-    )
+        max_length=1, choices=OPCOES_PAGAMENTO, default=STATUS_PAGAMENTO_PENDENTE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT) # protege os pedidos caso a classe Cliente seja deletada
+
+
+class Produtos_Pedidos(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.PROTECT)
+    produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
+    quantidade = models.PositiveSmallIntegerField() #protege contra números negativos
+    preco_unitario = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+# Caso a Classe Cliente seja deletada, o atributo cliente também será deletado
+class Endereco(models.Model):
+    rua = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    cliente = models.ForeignKey(
+        Cliente, on_delete=models.CASCADE) #podem ser inseridos vários endereços para o mesmo cliente
+
+
+class Carrinho(models.Model):
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
+
+
+class Itens_carrinho(models.Model):
+    carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveSmallIntegerField()
